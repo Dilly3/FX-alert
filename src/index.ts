04d7437be4d527a62352a75736a7b68bffa970dd
michael anikammadu <dil.anikamadu@gmail.com>
2025-07-20 12:01:53 +0100

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,11 @@
-
- 
-import express, { Request, Response } from 'express';
-import { getdb } from './datastore/store';
-import { Currency } from './datastore/currency_store';
+import express from 'express';
 require('dotenv').config()
-
-const currency = {
-    code : "NGN",
-    name : "Nigerian Naira",
-    symbol : "₦"
-}
 
 
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-const db = getdb()
-const currencyDb = new Currency(db)
-
-
-
-
-app.get('/', (req: Request, res: Response) => {
-  currencyDb.saveCurrency(currency).then((resp)=>{
-    console.log("THIS IS THE RESP",resp.id)
-res.json({
-    message : "Currency saved",
-    data : resp
-})
-}).catch((err)=>{
-    console.log("THIS IS THE ERR",err)
-        res.json({
-            message : "Currency not saved",
-            data : err
-        })
-})
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
