5a1118dbac7639e5636ee1af7e1a05844407a1fc
michael anikammadu <dil.anikamadu@gmail.com>
2025-07-22 10:10:45 +0100

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
+
+declare GOOGLE_CLOUD_PROJECT=$1
+declare ENV=$2
+
+read -p "Enter project ID: " GOOGLE_CLOUD_PROJECT
+read -p "Enter project Environment- prod or dev: " ENV
+
+
 declare PROJECT_NAME
 declare WORK_DIR="$(dirname "$(readlink -f $0)")"
 declare FOLDER_NAME="${WORK_DIR##*/}"
@@ -11,7 +19,17 @@ fi
 echo "deploying to $PROJECT_NAME"
 
 echo "deployment with buildpacks..."
-gcloud run deploy $PROJECT_NAME --source . --region us-west1 --allow-unauthenticated --platform managed --timeout=900 
+gcloud run deploy $PROJECT_NAME --source . --region us-west1 --allow-unauthenticated --platform managed \
+  --vpc-connector=projects/fs-alert-d4f21/locations/us-west1/connectors/fs-alert-vpc-connector \
+  --vpc-egress=private-ranges-only \
+  --region=us-west1 \
+  --memory=128Mi \
+  --cpu=1 \
+  --timeout=3000 \
+  --max-instances=2 --set-env-vars GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT \
+  --set-env-vars ENV=$ENV \
+  --region us-west1 \
+  --allow-unauthenticated
 
 
 echo "deployed to $PROJECT_NAME"
