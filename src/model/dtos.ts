da49928ca375cd5e49e4e279735565c454fdbdeb
michael anikammadu <dil.anikamadu@gmail.com>
2025-07-22 10:09:39 +0100

    add user dto

---
+++ src/model/dtos.ts
@@ -0,0 +1,10 @@
+type UserCreateDto = {
+    email: string
+    firstName: string 
+    lastName: string
+    fromCurrency: string
+    toCurrency: string
+    preferences: string
+}
+
+export {UserCreateDto}
