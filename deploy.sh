68edc3f86de77994b4661f05dc0cda104f1a109a
michael anikammadu <dil.anikamadu@gmail.com>
2025-07-30 02:14:06 +0100

    increase app memory to 512

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 declare GOOGLE_CLOUD_PROJECT=$1
 declare ENV=$2
+declare GCP_APPLICATION_SECRET_ID=$3
 
 read -p "Enter project ID: " GOOGLE_CLOUD_PROJECT
 read -p "Enter project Environment- prod/dev/sandbox: " ENV
-
 
 declare PROJECT_NAME
 declare WORK_DIR="$(dirname "$(readlink -f $0)")"
@@ -28,7 +28,7 @@ gcloud run deploy $PROJECT_NAME --source . --region us-west1 --allow-unauthentic
   --vpc-egress=private-ranges-only \
   --region=us-west1 \
   --memory=512Mi \
-  --cpu=1 \
+  --cpu=2 \
   --timeout=3600 \
   --max-instances=2 --set-env-vars GOOGLE_CLOUD_PROJECT=$GOOGLE_CLOUD_PROJECT \
   --set-env-vars ENV=$ENV \
