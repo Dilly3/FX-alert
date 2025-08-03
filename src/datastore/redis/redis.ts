e5f24a4e65f1385644e891ccab32f8f5e5f7e50b
michael anikammadu <dil.anikamadu@gmail.com>
2025-08-03 03:37:44 +0100

    fix redis error

--- src/datastore/redis/redis.ts
+++ src/datastore/redis/redis.ts
@@ -1,129 +1,160 @@
-import { createClient, RedisClientType } from 'redis';
-import { ConvertCurrencyRequest, ConvertCurrencyResponse, LiveRatesRequest, LiveRatesResponse } from '../../model/dtos';
-import { LogError, LogInfo } from '../../logger/gcp_logger';
+import { createClient, RedisClientType } from "redis";
+import {
+  ConvertCurrencyRequest,
+  ConvertCurrencyResponse,
+  LiveRatesRequest,
+  LiveRatesResponse,
+} from "../../model/dtos";
+import { LogError, LogInfo } from "../../logger/gcp_logger";
 
 export class RedisClient {
-    private client: RedisClientType;
-    private readonly CACHE_TTL: number;
-    private isUp: boolean;
-    constructor(host: string, port: number, password: string, username: string, ttl: number) {
-      
-        const {client, CACHE_TTL, isUp} = this.connect(username, password, host, port, ttl); 
-        this.client = client;
-        this.CACHE_TTL = CACHE_TTL;
-        this.isUp = isUp;
+  private client: RedisClientType;
+  private readonly CACHE_TTL: number;
+  private isUp: boolean;
+  constructor(
+    host: string,
+    port: number,
+    password: string,
+    username: string,
+    ttl: number
+  ) {
+    const { client, CACHE_TTL, isUp } = this.connect(
+      username,
+      password,
+      host,
+      port,
+      ttl
+    );
+    this.client = client;
+    this.CACHE_TTL = CACHE_TTL;
+    this.isUp = isUp;
+  }
+
+  private connect(
+    username: string,
+    password: string,
+    host: string,
+    port: number,
+    ttl: number
+  ): { client: RedisClientType; CACHE_TTL: number; isUp: boolean } {
+    let client: RedisClientType = null!;
+    let isUp: boolean = false;
+    let CACHE_TTL: number = 0;
+    try {
+      CACHE_TTL = ttl * 60 * 60 * 1000;
+      client = createClient({
+        username: username,
+        password: password,
+        socket: {
+          host: host,
+          port: port,
+        },
+      });
+
+      client.on("error", (error: Error) => {
+        LogError("Redis connection error:", { error: error });
+        isUp = false;
+      });
+
+      client.on("connect", () => {
+        LogInfo("Redis connected successfully", {});
+        isUp = true;
+      });
+
+      client.on("close", () => {
+        LogInfo("Redis connection closed", {});
+        isUp = false;
+      });
+
+      // Connect to Redis
+      client
+        .connect()
+        .then(() => {
+          LogInfo("Redis connected successfully", {});
+          isUp = true;
+        })
+        .catch((error: Error) => {
+          LogError("Error connecting to Redis", error);
+          isUp = false;
+        });
+
+      return { client, CACHE_TTL, isUp };
+    } catch (error) {
+      LogError("Error connecting to Redis", error);
+      throw error;
     }
+  }
 
-    private connect(username : string, password : string, host : string, port : number, ttl : number) : {client : RedisClientType, CACHE_TTL : number, isUp : boolean} {
-        let client : RedisClientType = null!;
-        let isUp : boolean = false;
-        let CACHE_TTL : number = 0;
-        try {
-            CACHE_TTL = ttl * 60 * 60 * 1000;
-            client = createClient({
-                username: username,
-                password: password,
-                socket: {
-                    host: host,
-                    port: port
-                }
-            });
+  async setCurrencyRate(
+    request: LiveRatesRequest,
+    value: LiveRatesResponse
+  ): Promise<void> {
+    const cacheKey = Array.isArray(request.currencies)
+      ? `currency_rate_${request.base}_${request.currencies.sort().join(",")}`
+      : `currency_rate_${request.base}_${request.currencies}`;
 
-            client.on('error', (error : Error) => {
-                LogError('Redis connection error:', {error: error});
-                isUp = false;
-            });
+    await this.set<LiveRatesResponse>(cacheKey, value);
+  }
 
-            client.on('connect', () => {
-                LogInfo('Redis connected successfully', {});
-                isUp = true;
-            });
+  async getCurrencyRate(
+    request: LiveRatesRequest
+  ): Promise<LiveRatesResponse | null> {
+    const cacheKey = Array.isArray(request.currencies)
+      ? `currency_rate_${request.base}_${request.currencies.sort().join(",")}`
+      : `currency_rate_${request.base}_${request.currencies}`;
 
-            client.on('close', () => {
-                LogInfo('Redis connection closed', {});
-                isUp = false;
-            });
+    return await this.get<LiveRatesResponse>(cacheKey);
+  }
 
-            // Connect to Redis
-            client.connect().then(() => {
-                LogInfo('Redis connected successfully', {});
-                isUp = true;
-            }).catch((error: Error) => {
-                LogError('Error connecting to Redis', error);
-                isUp = false;
-            });
+  async deleteCurrencyRate(request: LiveRatesRequest) {
+    const cacheKey = Array.isArray(request.currencies)
+      ? `currency_rate_${request.base}_${request.currencies.sort().join(",")}`
+      : `currency_rate_${request.base}_${request.currencies}`;
+    await this.delete(cacheKey);
+  }
 
-            return {client, CACHE_TTL, isUp};
-        } catch (error) {
-            LogError('Error connecting to Redis', error);
-            throw error;
-        }
-    }
+  async setConvertCurrency(
+    request: ConvertCurrencyRequest,
+    value: ConvertCurrencyResponse
+  ): Promise<void> {
+    const cacheKey = `currency_rate_${request.from}_${request.to}_${request.amount}_${request.date}`;
+    await this.set<ConvertCurrencyResponse>(cacheKey, value);
+  }
 
+  async getConvertCurrency(
+    request: ConvertCurrencyRequest
+  ): Promise<ConvertCurrencyResponse | null> {
+    const cacheKey = `currency_rate_${request.from}_${request.to}_${request.amount}_${request.date}`;
+    return await this.get<ConvertCurrencyResponse>(cacheKey);
+  }
 
-
-    async setCurrencyRate(request : LiveRatesRequest , value : LiveRatesResponse) : Promise<void>
-    {
-        const cacheKey = `currency_rate_${request.base}_${request.currencies.join(',')}`;
-        await this.set<LiveRatesResponse>(cacheKey, value);
-    }
-
-    async getCurrencyRate(request : LiveRatesRequest ) : Promise<LiveRatesResponse | null>
-    {
-        const cacheKey = `currency_rate_${request.base}_${request.currencies.join(',')}`;
-        return await this.get<LiveRatesResponse>(cacheKey);
-    }
-
-    async deleteCurrencyRate(request : LiveRatesRequest)
-    {
-        const cacheKey = `currency_rate_${request.base}_${request.currencies.join(',')}`;
-        await this.delete(cacheKey);
-    }
-
-async setConvertCurrency(request: ConvertCurrencyRequest, value : ConvertCurrencyResponse): Promise<void> {
- const cacheKey = `currency_rate_${request.from}_${request.to}_${request.amount}_${request.date}`;
-        await this.set<ConvertCurrencyResponse>(cacheKey, value);
-}
-
-    async getConvertCurrency(request : ConvertCurrencyRequest) : Promise<ConvertCurrencyResponse | null>
-    {
-const cacheKey = `currency_rate_${request.from}_${request.to}_${request.amount}_${request.date}`;
-return await this.get<ConvertCurrencyResponse>(cacheKey)
-    }
-
-
-    isConnected() : boolean {
-        return this.isUp;
-    }
-private async get<T>(key: string): Promise<T | null> {
+  isConnected(): boolean {
+    return this.isUp;
+  }
+  private async get<T>(key: string): Promise<T | null> {
     const value: string | null = await this.client.get(key);
     if (!value) return null;
-    
+
     try {
-        return JSON.parse(value) as T;
+      return JSON.parse(value) as T;
     } catch (error) {
-        console.error('Failed to parse cached value:', error);
-        return null;
+      console.error("Failed to parse cached value:", error);
+      return null;
     }
-}
+  }
 
-private async set<T>(key: string, value: T): Promise<void> {
+  private async set<T>(key: string, value: T): Promise<void> {
     try {
-        // Convert milliseconds to seconds
-        const ttlSeconds = Math.floor(this.CACHE_TTL / 1000);
-        await this.client.set(key, JSON.stringify(value), { EX: ttlSeconds });
+      // Convert milliseconds to seconds
+      const ttlSeconds = Math.floor(this.CACHE_TTL / 1000);
+      await this.client.set(key, JSON.stringify(value), { EX: ttlSeconds });
     } catch (error) {
-        console.error('Failed to set cache value:', error);
-        throw error;
+      console.error("Failed to set cache value:", error);
+      throw error;
     }
+  }
+
+  private async delete(key: string) {
+    await this.client.del(key);
+  }
 }
-
-    private async delete(key: string) {
-        await this.client.del(key);
-    }
-
- 
-}
-
-
-
