09d08be6e4f8d0055ce24c7737dc44c4f6b18b66
michael anikammadu <dil.anikamadu@gmail.com>
2025-08-11 14:19:29 +0100

    refactor artifact-registry

--- terraform/artifact-registry/main.tf
+++ terraform/artifact-registry/main.tf
@@ -2,7 +2,7 @@
 resource "google_artifact_registry_repository" "fx_alert_cloud_run_source_deploy" {
   location      = var.region
 project = var.project_id
-  repository_id = "fx-alert-cloud-run-source-deploy"
+  repository_id = var.repository
   description   = "docker repository"
   format        = "DOCKER"
 }
