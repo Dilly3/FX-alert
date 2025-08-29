a97658ab5623c18f9235fdc87cd344ab0290a09d
michael anikammadu <dil.anikamadu@gmail.com>
2025-08-29 01:38:39 +0100

    migrate cloud run project

--- terraform/postgres/clear_cache.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf .terraform
-rm .terraform.lock.hcl
-
-# Delete the state file
-gsutil rm gs://fx-alert-tfstate-bucket/terraform/postgres/state/default.tfstate
-
-terraform init
